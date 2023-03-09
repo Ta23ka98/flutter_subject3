9dd8b3fb21e779d27cc9ca55401440a8402ee42d
まさたか <shinonome419@icloud.com>
2023-03-09 14:25:17 +0900

    add: 初期のTodoリストを追加

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_subject3/todo.dart';
+
+///const List<Todo>と、 const[]の違い
+List<Todo> todoList = const [
+  Todo(id: 1, description: "task1", isCompleted: false),
+  Todo(id: 2, description: "task2", isCompleted: false),
+  Todo(id: 3, description: "task3", isCompleted: false),
+  Todo(id: 4, description: "task4", isCompleted: false),
+  Todo(id: 5, description: "task5", isCompleted: false),
+];
